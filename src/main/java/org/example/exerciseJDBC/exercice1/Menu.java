424d6fd509032631709b6a4e5a2a052e13679d30
Sal <user@example.com>
2025-05-22 16:50:49 +0200

    Update exercice JDBC

--- src/main/java/org/example/exerciseJDBC/exercice1/Menu.java
+++ src/main/java/org/example/exerciseJDBC/exercice1/Menu.java
@@ -3,10 +3,7 @@ package org.example.exerciseJDBC.exercice1;
 
 import org.example.exerciseJDBC.exercice1.utils.ConnectionUtils;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Menu {
@@ -37,13 +34,80 @@ public class Menu {
 
                 case 2:
 
+                    try {
+                        connection = ConnectionUtils.getSQLConnection();
+                        String request2 = "SELECT * FROM students";
+                        Statement statement = connection.createStatement();
+
+                        ResultSet resultSet = statement.executeQuery(request2);
+                        System.out.println("Voici la liste des étudiants : ");
+                        while (resultSet.next()) {
+                            System.out.print(" Prenom : " + resultSet.getString("first_name"));
+                            System.out.print(" Nom : " + resultSet.getString("last_name"));
+                        }
+                    } catch (SQLException e) {
+                        System.out.println(e.getMessage());
+                    } finally {
+                        try {
+                            connection.close();
+
+                        } catch (SQLException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+
                     break;
 
                 case 3:
 
+                    try {
+                        connection = ConnectionUtils.getSQLConnection();
+                        String request1 = "SELECT * FROM students where class_id = ? ";
+                        PreparedStatement statement = connection.prepareStatement(request1);
+
+                        System.out.println("Entrez le numéro de la classe recherché : ");
+                        int getId = scanner.nextInt();
+                        statement.setInt(1, getId);
+                        System.out.println("Voici la liste des étudiants dans le numéro de classe " + getId + " : ");
+                        ResultSet resultSet = statement.executeQuery();
+                        while (resultSet.next()) {
+                            System.out.println(resultSet.getString("first_name"));
+                        }
+                    } catch (SQLException e) {
+                        System.out.println(e.getMessage());
+                    } finally {
+                        try {
+                            connection.close();
+
+                        } catch (SQLException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+
                     break;
 
                 case 4:
+
+                    try {
+                        connection = ConnectionUtils.getSQLConnection();
+                        String request3 = "DELETE FROM students WHERE id = ? ";
+                        PreparedStatement statement = connection.prepareStatement(request3);
+
+                        System.out.println("Entrez l'id de l'étudiant : ");
+                        int getId = scanner.nextInt();
+                        statement.setInt(1, getId);
+                        System.out.println("Vous avez supprimez l'étudiant " + getId);
+
+                    }catch (SQLException e){
+                        System.out.println(e.getMessage());
+                    } finally {
+                        try {
+                            connection.close();
+
+                        } catch (SQLException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
 
                     break;
 
